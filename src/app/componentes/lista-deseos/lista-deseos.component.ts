c4dc30b91e6cd258100393a2212b83aa966d2f14
Emanuel Contreras <emanuelcf@outlook.es>
2022-07-02 07:40:18 -0600

    ExamenDWII

--- src/app/componentes/lista-deseos/lista-deseos.component.ts
+++ src/app/componentes/lista-deseos/lista-deseos.component.ts
@@ -14,7 +14,7 @@ export class ListaDeseosComponent implements OnInit {
 
   Existencias:Boolean;
   Carga: Boolean;
-  ListaDeseos: any[number];
+  ListaDeseos: any[];
   Usuario:any;
   constructor(private router:Router, public _servicio : InfoPaginaService,private http: HttpClient) {
     this.ListaDeseos=new Array;
@@ -29,11 +29,25 @@ export class ListaDeseosComponent implements OnInit {
 
     //carga la lista de deseos
     _servicio.GetListaDeseos(this.Usuario).then(async(Producto:Array<any>)=>{
-      if (Producto!=null && Object.values(Producto).length) {
-        await Object.values(Producto).forEach( async element => {
-          //espera a cargar los productos
-          await this.AgregarProducto(element);
+        if (Producto!=null && Object.values(Producto).length) {
+        Object.values(Producto).forEach( async element => {
+         
+          var obj: any= _servicio.InfoProd[element];
+          obj.index=element;
+          obj.EstrellasA=Array(obj.Valoracion).fill(0)
+          obj.EstrellasN=Array(5-obj.Valoracion).fill(0)
+          this.ListaDeseos.push(obj)
         });
+        this.ListaDeseos.sort((a,b)=>{
+          if (a.Valoracion<b.Valoracion) {
+            return 1;
+          }
+
+          if (a.Valoracion>b.Valoracion) {
+            return -1;
+          }
+          return 0
+        })
         console.log(this.ListaDeseos)
         this.Existencias=true
         this.Carga=true;
@@ -57,6 +71,8 @@ export class ListaDeseosComponent implements OnInit {
       var obj: any=resp;
       obj.index=ID;
       this.ListaDeseos.push(obj)
+      
+      console.log(ID)
     });
 
   }
